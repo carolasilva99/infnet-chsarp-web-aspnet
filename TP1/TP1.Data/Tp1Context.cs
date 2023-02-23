7fe9285024a3f1c6745b30001ce10872cf742a4a
Carol Alves <carolasilva99@gmail.com>
2023-02-23 09:49:25 -0300

    Add project files.

---
+++ TP1/TP1.Data/Tp1Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using TP1.Domain;
+
+namespace TP1.Data;
+
+public class Tp1Context : DbContext
+{
+    public Tp1Context(DbContextOptions<Tp1Context> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Friend> Friends { get; set; }
+}
---
+++ TP1/TP1.Domain/Friend.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TP1.Domain
+{
+    public class Friend
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+    }
+}
